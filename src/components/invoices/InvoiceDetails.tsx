5c12044ae6d3612151bd8b9032bcdfcb8ceb3e0e
Abdulrahman Adekeye <58373414+Abdulrahaman1000@users.noreply.github.com>
2026-01-17 06:15:50 -0800

    updateVercel

--- src/components/invoices/InvoiceDetails.tsx
+++ src/components/invoices/InvoiceDetails.tsx
@@ -76,7 +76,7 @@ const InvoiceDetails: React.FC<InvoiceDetailsProps> = ({ invoice }) => {
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
               <tr>
-                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                <th className="px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase">
                   Description
                 </th>
                 <th className="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase">
